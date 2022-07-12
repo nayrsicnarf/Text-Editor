d65584ac6335a0e594084012ddbce8c125d01e02
Francis Aguinaldo <fryaguinaldo@gmail.com>
2022-07-12 12:10:27 -0700

    updatesd src-sw.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,24 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-// Export a function we will use to POST to the database
+// Export a function to POST to the database
 export const putDb = async (content) => {
   console.log("Post to the database");
   console.log(jateDb);
 
-  // Create a connection to the database database and version we want to use
+  // Create a connection to the database and the version we want to use
   const jateDb = await openDB("jate", 1);
 
   // Create a new transaction and specify the database and data privileges
   const jx = jateDb.transaction("jate", "readwrite");
 
-  // Open up the desired object store
+  // Open up the object store
   const store = jx.objectStore("jate");
 
-  // Use the .put() method on the store and pass in the content.
+  // Use the .put() method on the store and pass in the content
   const request = store.put({ jate: content });
 
-  // Get confirmation of the request.
+  // Get confirmation of the request
   const result = await request;
   console.log("data saved to the database", result);
 
@@ -38,18 +38,18 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-// Export a function we will use to GET to the database
+// Export a function to GET to the database
 export const getDb = async () => {
   console.log("GET from the database");
   console.log(jateDb);
 
-  // Create a connection to the database database and version we want to use
+  // Create a connection to the database and version we want to use
   const jateDb = await openDB("jate", 1);
 
   // Create a new transaction and specify the database and data privileges
   const jx = jateDb.transaction("jate", "readonly");
 
-  // Open up the desired object store
+  // Open up the object store
   const store = jx.objectStore("jate");
 
   // Use the .getAll() method to get all data in the database
@@ -59,7 +59,7 @@ export const getDb = async () => {
   const result = await request;
   console.log("result.value", result);
   return result;
-  
+
   // console.error("getDb not implemented");
 };
 
