af5372ba7502715da93826592b0841d07f768d91
Francis Aguinaldo <fryaguinaldo@gmail.com>
2022-07-12 20:58:41 -0700

    updated package.json

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -58,7 +58,7 @@ export const getDb = async () => {
   // Get confirmation of the request
   const result = await request;
   console.log("result.value", result);
-  return result;
+  // return result;
 
   // console.error("getDb not implemented");
 };
